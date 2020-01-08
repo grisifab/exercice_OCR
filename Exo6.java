e52fa6084a3514072f3ef0d1482f961044fbd9ee
grisifab <fabrice.grisi@laposte.net>
2020-01-08 19:12:06 +0100

    rajout de l'exercice 6

---
+++ Exo6.java
@@ -0,0 +1,28 @@
+package exo6;
+
+public class Exo6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Écrire un programme Java pour tester si un tableau contient une valeur spécifique
+		int[] tabIn = {12,24,56,11,14,12,0,4,9,10};
+		int ind = -12;
+		int valeurSpe = 25;
+		
+		for(int i=0; i < tabIn.length; i++) {
+			if (tabIn[i] == valeurSpe){
+				ind = i;
+			}	
+		}
+		
+		if (ind != -12){
+			System.out.println("La valeur spéciale : " + valeurSpe + " est presente à l'indice " + ind + " du tableau");
+		} else {
+			System.out.println("La valeur spéciale : " + valeurSpe + " n'est pas presente dans le tableau");
+		}
+		
+	}
+
+}
